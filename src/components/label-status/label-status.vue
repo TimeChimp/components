3e4f97f7e2afbd3283b2f47fcd1be77967af2e0a
Hidde Ottens <hidde95@gmail.com>
2019-10-28 15:40:52 +0100

    Add label status component

---
+++ src/components/label-status/label-status.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="label-status"
+    :class="{
+      'label-status__registered': status === 0,
+      'label-status__pending-approval': status === 1,
+      'label-status__approved': status === 2,
+      'label-status__rejected': status === 3,
+      'label-status__invoiced': status === 4,
+      'label-status__written-off': status === 5
+    }"
+  >
+    <span v-if="status === 0">{{ "Registered" }}</span>
+    <span v-else-if="status === 1">{{ "Pending approval" }}</span>
+    <span v-else-if="status === 2">{{ "Approved" }}</span>
+    <span v-else-if="status === 3">{{ "Rejected" }}</span>
+    <span v-else-if="status === 4">{{ "Invoiced" }}</span>
+    <span v-else-if="status === 5">{{ "Written off" }}</span>
+  </div>
+</template>
+
+<script lang="ts" src="./label-status.ts"></script>
+
+<style lang="scss" src="./label-status.scss"></style>
