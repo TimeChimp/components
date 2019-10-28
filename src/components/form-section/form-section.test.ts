77049bced30742ca623ace7f01895d8c046a91f8
Hidde Ottens <hidde95@gmail.com>
2019-10-28 15:22:31 +0100

    Add label component

--- src/components/form-section/form-section.test.ts
+++ src/components/form-section/form-section.test.ts
@@ -11,7 +11,24 @@ const factory = (values = {}) => {
 
 describe("Form section", () => {
   it("renders a component", () => {
-    const wrapper = factory();
+    const wrapper = factory({
+      title: "Test title"
+    });
     expect(wrapper.find(".form-section").exists()).toBe(true);
+  });
+
+  it("renders a title", () => {
+    const wrapper = factory({
+      title: "Test title"
+    });
+    expect(wrapper.find(".form-section__title").text()).toEqual("Test title");
+  });
+
+  it("renders a small size", () => {
+    const wrapper = factory({
+      title: "Test title",
+      size: "s"
+    });
+    expect(wrapper.find(".col-5").exists()).toBe(true);
   });
 });
