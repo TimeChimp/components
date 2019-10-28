71353c9098b0a90051990976abeb5cc4e5a77daa
Hidde Ottens <hidde95@gmail.com>
2019-10-28 14:34:24 +0100

    Update button round component

--- src/components/button-round/button-round.ts
+++ src/components/button-round/button-round.ts
@@ -1,6 +1,10 @@
-import { Component } from "vue-property-decorator";
+import { Component, Prop } from "vue-property-decorator";
 
 import { TcButton } from "../button";
 
 @Component({})
-export default class TcButtonRound extends TcButton {}
+export default class TcButtonRound extends TcButton {
+  @Prop() private buttonClass!: string;
+  @Prop() private loading!: boolean;
+  @Prop() private disabled!: boolean;
+}
--- src/components/button-round/button-round.vue
+++ src/components/button-round/button-round.vue
@@ -1,14 +1,8 @@
 <template>
   <button
-    :type="type"
     class="button button-round"
     :disabled="disabled || loading"
-    :class="{
-      button__square: square,
-      button__primary: primary,
-      button__secondary: secondary,
-      button__active: active
-    }"
+    :class="buttonClass"
     @click="$emit('click')"
   >
     <slot v-if="!loading" />
