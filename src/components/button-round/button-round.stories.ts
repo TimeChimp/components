9dff03beaa55b1ca76c51b5643eef26104e3f981
Hidde Ottens <hidde95@gmail.com>
2019-10-28 10:35:17 +0100

    Add button round stories

--- src/components/button-round/button-round.stories.ts
+++ src/components/button-round/button-round.stories.ts
@@ -4,14 +4,52 @@ import { storiesOf } from "@storybook/vue";
 import TcButtonRound from "./button-round.vue";
 import TcIcon from "../icons/icon.vue";
 
-storiesOf("Buttons|Button round", module).add("Default", () => ({
-  components: {
-    TcButtonRound,
-    TcIcon
-  },
-  template: `
-            <tc-button-round>
-                <tc-icon name="close" />
-            </tc-button-round>
-    `
-}));
+storiesOf("Buttons|Button round", module)
+  .add("Default", () => ({
+    components: {
+      TcButtonRound,
+      TcIcon
+    },
+    template: `
+                    <tc-button-round />
+            `
+  }))
+  .add("Close", () => ({
+    components: {
+      TcButtonRound,
+      TcIcon
+    },
+    template: `
+                    <tc-button-round>
+                        <tc-icon name="close" />
+                    </tc-button-round>
+            `
+  }))
+  .add("Next", () => ({
+    components: {
+      TcButtonRound,
+      TcIcon
+    },
+    template: `
+                    <tc-button-round>
+                        <tc-icon 
+                            name="arrow"
+                            style="transform:rotate(-90deg)"
+                        />
+                    </tc-button-round>
+            `
+  }))
+  .add("Previous", () => ({
+    components: {
+      TcButtonRound,
+      TcIcon
+    },
+    template: `
+                    <tc-button-round>
+                        <tc-icon 
+                            name="arrow" 
+                            style="transform:rotate(90deg)"
+                        />
+                    </tc-button-round>
+            `
+  }));
