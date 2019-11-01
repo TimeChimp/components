ba5db1cd5dba20002f5a479734d8aef573d46796
Hidde Ottens <hidde95@gmail.com>
2019-11-01 09:43:45 +0100

    Add pagination component

---
+++ src/components/pagination/pagination.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="pagination">
+    <ul class="pagination__list">
+      <!--First-->
+      <li v-if="hasFirst">
+        <a
+          role="button"
+          href="#"
+          class="pagination__link"
+          @click.prevent="first"
+        >
+          1
+        </a>
+      </li>
+      <li v-if="hasFirstEllipsis">
+        <span class="pagination__ellipsis">&hellip;</span>
+      </li>
+
+      <!--Pages-->
+      <li v-for="page in pagesInRange" :key="page.number">
+        <a
+          href="#"
+          class="pagination__link"
+          :class="{ 'pagination__link--current': page.isCurrent }"
+          @click.prevent="page.click"
+        >
+          {{ page.number }}
+        </a>
+      </li>
+
+      <!--Last-->
+      <li v-if="hasLastEllipsis">
+        <span class="pagination__ellipsis">&hellip;</span>
+      </li>
+      <li v-if="hasLast">
+        <a href="#" class="pagination__link" @click.prevent="last">
+          {{ pageCount }}
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts" src="./pagination.ts"></script>
+
+<style lang="scss" src="./pagination.scss"></style>
