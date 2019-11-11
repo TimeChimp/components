8e7a0590499c6d806c6ba1d04ec0716a60484fc2
Hidde Ottens <hidde95@gmail.com>
2019-11-11 12:48:08 +0100

    Fix button round issue

--- src/components/button-round/button-round.ts
+++ src/components/button-round/button-round.ts
@@ -5,6 +5,5 @@ import { TcButton } from "../button";
 @Component({})
 export default class TcButtonRound extends TcButton {
   @Prop() private buttonClass!: string;
-  @Prop() private loading!: boolean;
   @Prop() private disabled!: boolean;
 }
